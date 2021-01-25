c81eabd2420beb9f09c418c657f7d9fe7d72212c
Dylan Rowe <dylanthk@gmail.com>
2021-01-25 13:05:39 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
+import React from 'react'
+import { useState } from 'react'
 import './App.css';
+import Body from  './illust57-4505.jpg'
+
 
 function App() {
+
+// const [localState, setLocalState] = useState({
+
+// })  
+
+
+const handleControlledInputChange = (e) => {
+var x = e.clientX;
+var y = e.clientY; 
+var coor = "X coords: " + x + ", Y coords: " + y;
+console.log(coor)
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onMouseMove={handleControlledInputChange} className="fullScreen">
+    <img className="bodyImage" src={Body}></img>
+ 
     </div>
   );
 }
